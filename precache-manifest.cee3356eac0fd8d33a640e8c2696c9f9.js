565c5d7aab7635b9bdc66bb079167f50648d06d3
zvttvt <39856382+hellotvt@users.noreply.github.com>
2019-01-30 12:55:32 +0700

    Add files via upload

---
+++ precache-manifest.cee3356eac0fd8d33a640e8c2696c9f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "57ad142f4b9e58b8d547",
+    "url": "/static/js/main.57ad142f.chunk.js"
+  },
+  {
+    "revision": "9915b26aa420f2cad06d",
+    "url": "/static/js/1.9915b26a.chunk.js"
+  },
+  {
+    "revision": "57ad142f4b9e58b8d547",
+    "url": "/static/css/main.2705edf4.chunk.css"
+  },
+  {
+    "revision": "363d8d8a308066149fbc4ea781e716c5",
+    "url": "/index.html"
+  }
+];
